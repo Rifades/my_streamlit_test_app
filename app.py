94b2ff335194d08e1df90200d8cf47501ed49493
Mir Rejaul Islam <114472389+Rifades@users.noreply.github.com>
2026-02-04 12:59:52 +0000

    second modification

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ if 'my_todo_list' not in st.session_state:
     st.session_state.my_todo_list = []
 col1, col2 = st.columns(2)
 with col1:
-    b = col1.text_input("Enter your todo item")
-    a = col1.button("Add")
+    with st.form('My Form', clear_on_submit=True):
+        b = st.text_input("Enter your todo item")
+        a = st.form_submit_button("Add")
+        if a and b:
+            st.session_state.my_todo_list.append(b)
     c = col1.button("Clear")
-
-if a and b:
-    st.session_state.my_todo_list.append(b)
-if c:
-    st.session_state.my_todo_list = []
+    if c:
+        st.session_state.my_todo_list = []
 with col2:
     col2.write("Your Todo List:")
     for i in st.session_state.my_todo_list:
